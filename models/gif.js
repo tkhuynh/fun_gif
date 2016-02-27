042d0ece8b84f3f56426179fc8364192c366292e
tkhuynh <thequest2311@yahoo.com>
2016-02-26 16:47:19 -0800

    set up client side, get results from Giphy API call show on view

--- models/gif.js
+++ models/gif.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 	Schema = mongoose.Schema;
 
 var GifSchema = new Schema( {
-	tag: String,
+	keyword: String,
 	url: String,
 	imported: Date
 });
---
+++ public/scripts/app.js
@@ -0,0 +1,47 @@
+var app = angular.module('funGifApp', ['ngRoute', 'ngResource']);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'templates/home.html',
+			controller: 'SearchCtrl'
+		})
+		.when('/favorites', {
+			templateUrl: 'templates/favorites.html',
+			controller: 'FavoritesCtrl'
+		});
+
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+}]);
+
+app.factory('Gif', ['$resource', function($resource) {
+	return $resource('/api/gifs/:id', {id: '@_id'});
+}]);
+
+app.controller('SearchCtrl', ['$scope', '$http', 'Gif', function ($scope, $http, Gif) {
+	$scope.gifs = [];
+	$scope.searchKeyword = function () {
+		var keyword = $scope.keyword;
+		$scope.savedKeyword = keyword;
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + keyword + '&api_key=dc6zaTOxFJmzC';
+		console.log(url);
+
+		$http.get(url)
+			.then(function (response) {
+				$scope.keyword = '';
+				// api return 25 gifs, but only need 24
+				response.data.data.pop();
+				$scope.gifs = response.data.data;
+				// find the minimum height of a gif to set height for the rest
+				$scope.minHeight = response.data.data.sort(function (a,b) {
+					return Number(a.images.downsized.height) - Number(b.images.downsized.height);
+				})[0].images.downsized.height + 'px';
+			}, function (error) {
+				console.log(error);
+			}
+		);
+	};
+}]);
