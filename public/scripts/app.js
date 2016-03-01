9204d92ed50a66309457e32ed37f8cf3a81ff25c
tkhuynh <thequest2311@yahoo.com>
2016-02-29 23:18:36 -0800

    set up auth in client side, still need to debug issue caused error for giphy api call when user logged in

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,9 @@
 function randomNum(array) {
 	return Math.floor(Math.random() * array.length);
 }
-var app = angular.module('funGifApp', ['ngRoute', 'ngResource']);
+var app = angular.module('funGifApp', ['ngRoute', 'ngResource', 'satellizer']);
 
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: 'templates/home.html',
@@ -12,6 +12,21 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 		.when('/favorites', {
 			templateUrl: 'templates/favorites.html',
 			controller: 'FavoritesCtrl'
+		})
+		.when('/signup', {
+			templateUrl: 'templates/signup.html',
+			controller: 'AuthCtrl'
+		})
+		.when('/login', {
+			templateUrl: 'templates/login.html',
+			controller: 'AuthCtrl'
+		})
+		.when('/profile', {
+			templateUrl: 'templates/profile.html',
+			controller: 'ProfileCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
 		});
 
 	$locationProvider.html5Mode({
@@ -21,70 +36,170 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 }]);
 
 app.factory('Gif', ['$resource', function($resource) {
-	return $resource('/api/gifs/:id', {id: '@_id'});
+	return $resource('/api/gifs/:id', {
+		id: '@_id'
+	});
 }]);
 
-app.controller('SearchCtrl', ['$scope', '$http', 'Gif', function ($scope, $http, Gif) {
+app.controller('MainCtrl', ['$scope', '$auth', '$http', '$location',
+	function($scope, $auth, $http, $location) {
+		$scope.isAuthenticated = function() {
+			// send GET request to '/api/me'
+			$http.get('/api/me')
+				.then(function(response) {
+					// if response.data comes back, set $scope.currentUser = response.data
+					if (response.data) {
+						$scope.currentUser = response.data;
+					} else {
+						// otherwise remove token (https://github.com/sahat/satellizer#authremovetoken)
+						$auth.removeToken();
+					}
+				}, function(error) {
+					console.error(error);
+					$auth.removeToken();
+				});
+		};
+
+		$scope.isAuthenticated();
+
+		$scope.logout = function() {
+			// logout (https://github.com/sahat/satellizer#authlogout)
+			$auth.logout()
+				.then(function() {
+					// set $scope.currentUser = null
+					$scope.currentUser = null;
+					// redirect to '/login'
+					$location.path('/login');
+				});
+		};
+	}
+]);
+
+app.controller('AuthCtrl', ['$scope', '$auth', '$location',
+	function($scope, $auth, $location) {
+		// if $scope.currentUser, redirect to '/profile'
+		if ($scope.currentUser) {
+			$location.path('/profile');
+		}
+
+		// clear sign up / login forms
+		$scope.user = {};
+
+		$scope.signup = function() {
+			// signup (https://github.com/sahat/satellizer#authsignupuser-options)
+			$auth.signup($scope.user)
+				.then(function(response) {
+					// set token (https://github.com/sahat/satellizer#authsettokentoken)
+					$auth.setToken(response.data.token);
+					// call $scope.isAuthenticated to set $scope.currentUser
+					$scope.isAuthenticated();
+					// clear sign up form
+					$scope.user = {};
+					// redirect to '/profile'
+					$location.path('/profile');
+				}, function(error) {
+					console.error(error);
+				});
+		};
+
+		$scope.login = function() {
+			// login (https://github.com/sahat/satellizer#authloginuser-options)
+			$auth.login($scope.user)
+				.then(function(response) {
+					// set token (https://github.com/sahat/satellizer#authsettokentoken)
+					$auth.setToken(response.data.token);
+					// call $scope.isAuthenticated to set $scope.currentUser
+					$scope.isAuthenticated();
+					// clear sign up form
+					$scope.user = {};
+					// redirect to '/profile'
+					$location.path('/profile');
+				}, function(error) {
+					console.error(error);
+				});
+		};
+	}
+]);
+
+app.controller('ProfileCtrl', ['$scope', '$auth', '$http', '$location',
+	function($scope, $auth, $http, $location) {
+		// if user is not logged in, redirect to '/login'
+		if ($scope.currentUser === undefined) {
+			$location.path('/login');
+		}
+
+		$scope.editProfile = function() {
+			$http.put('/api/me', $scope.currentUser)
+				.then(function(response) {
+					$scope.showEditForm = false;
+				}, function(error) {
+					console.error(error);
+					$auth.removeToken();
+				});
+		};
+	}
+]);
+
+app.controller('SearchCtrl', ['$scope', '$http', 'Gif', function($scope, $http, Gif) {
 	var greetings = ['hello', 'nice day', 'good', 'nice', 'cute', 'thumb up', 'love', 'happy'];
 	$scope.gifs = [];
 	$scope.searched = false;
 	var keyword = greetings[randomNum(greetings)];
 	var url = 'https://api.giphy.com/v1/gifs/search?q=' + keyword + '&api_key=dc6zaTOxFJmzC';
 	$http.get(url)
-			.then(function (response) {
-				var data = response.data.data;
-				// only need to return the number of gifs which can be divided by 4
-				if (data.length / 4 >= 1 && data.length % 4 !== 0 ) {
-					for (var counter = 0; counter < (data.length % 4); counter ++) {
-						data.pop();
-					}
+		.then(function(response) {
+			var data = response.data.data;
+			// only need to return the number of gifs which can be divided by 4
+			if (data.length / 4 >= 1 && data.length % 4 !== 0) {
+				for (var counter = 0; counter < (data.length % 4); counter++) {
+					data.pop();
 				}
-				$scope.gifs = data;
-				function getMinHeight() {
-					// find the minimum height of a gif to set height for the rest
-					if (data.length > 0) {
-						$scope.minHeight = $scope.gifs.sort(function (a,b) {
-							return Number(a.images.downsized.height) - Number(b.images.downsized.height);
-						})[0].images.downsized.height;
-						if ($scope.minHeight == 0) {
-							$scope.minHeight = "100 px";
-						} else {
-							$scope.minHeight = $scope.minHeight + "px";
-						}
-					}
-				}
-				getMinHeight();
-			}, function (error) {
-				console.log(error);
 			}
-		);
+			$scope.gifs = data;
 
-	$scope.searchKeyword = function () {
+			function getMinHeight() {
+				// find the minimum height of a gif to set height for the rest
+				if (data.length > 0) {
+					$scope.minHeight = $scope.gifs.sort(function(a, b) {
+						return Number(a.images.downsized.height) - Number(b.images.downsized.height);
+					})[0].images.downsized.height;
+					if ($scope.minHeight == 0) {
+						$scope.minHeight = "100 px";
+					} else {
+						$scope.minHeight = $scope.minHeight + "px";
+					}
+				}
+			}
+			getMinHeight();
+		}, function(error) {
+			console.log(error);
+		});
+
+	$scope.searchKeyword = function() {
 		keyword = $scope.keyword;
 		$scope.savedKeyword = keyword;
 		url = 'https://api.giphy.com/v1/gifs/search?q=' + keyword + '&api_key=dc6zaTOxFJmzC';
 		console.log(url);
 
 		$http.get(url)
-			.then(function (response) {
+			.then(function(response) {
 				$scope.keyword = '';
 				$scope.searched = true;
 				var data = response.data.data;
 				// only need to return the number of gifs which can be divided by 4
-				if (data.length / 4 >= 1 && data.length % 4 !== 0 ) {
-					for (var counter = 0; counter < (data.length % 4); counter ++) {
+				if (data.length / 4 >= 1 && data.length % 4 !== 0) {
+					for (var counter = 0; counter < (data.length % 4); counter++) {
 						data.pop();
 					}
 				}
 				$scope.gifs = data;
 				getMinHeight();
-			}, function (error) {
+			}, function(error) {
 				console.log(error);
-			}
-		);
+			});
 	};
 
-	$scope.saveGif = function (gif) {
+	$scope.saveGif = function(gif) {
 		gif.favorited = true;
 		timeStamp = new Date();
 		timeStamp = timeStamp.toLocaleDateString();
@@ -94,25 +209,25 @@ app.controller('SearchCtrl', ['$scope', '$http', 'Gif', function ($scope, $http,
 			imported: timeStamp,
 			height: Number(gif.images.downsized.height)
 		};
-		Gif.save(gifData, function (data) {
+		Gif.save(gifData, function(data) {
 			console.log("success");
-		}, function (error) {
+		}, function(error) {
 			console.log(error);
 		});
 	};
 }]);
 
-app.controller('FavoritesCtrl',['$scope', '$http', function ($scope, $http) {
+app.controller('FavoritesCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.favorites = [];
 	$http.get('/api/gifs')
-	 .then(function (response) {
-	 	$scope.favorites = response.data.results;
-	 	if (response.data.results.length > 0) {
-			$scope.minHeight = $scope.favorites.sort(function (a,b) {
-				return a.height - b.height;
-			})[0].height + 'px';
-		}
-	 }, function (error) {
-	 	console.log(error);
-	 });
+		.then(function(response) {
+			$scope.favorites = response.data.results;
+			if (response.data.results.length > 0) {
+				$scope.minHeight = $scope.favorites.sort(function(a, b) {
+					return a.height - b.height;
+				})[0].height + 'px';
+			}
+		}, function(error) {
+			console.log(error);
+		});
 }]);
